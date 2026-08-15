3b4300822e913746f93b76ba990c889c6a0c4370
kangaroomeat <121744710+kangaroomeat@users.noreply.github.com>
2026-08-14 20:55:15 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var toDoSection = document.querySelector(".to-do-section");
+var inProgressSection = document.querySelector(".in-progress-section");
 var testBtn = document.querySelector(".test-button");
 
 function addItem(text){
     const para = document.createElement('p');
     para.innerText = text;
     toDoSection.appendChild(para);
+    para.setAttribute("contentEditable", "true");
+    para.style.backgroundColor = "white";
+    para.style.margin = "10px";
+
+    para.onclick=function(){
+        console.log(this.innerText);
+    }
+
+    para.ondblclick=function(){
+        //this.remove();
+        inProgressSection.appendChild(this);
+        
+    }
+
+
 }
+
+
 
 testBtn.onclick=function(){
     console.log("works");
-    addItem("dog");
+    addItem(" ");
 }
+
